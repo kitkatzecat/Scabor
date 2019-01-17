46e3250aa9b170e7f93ec8da2e60c6d589a99a0d
Bryce <kitkatzecat@outlook.com>
2019-01-16 19:19:57 -0600

    Added Game.Cursor

---
+++ resources/js/cursor.js
@@ -0,0 +1,26 @@
+Game.Cursor = {
+	MouseMove: function(e) {
+		Game.Cursor.Composition.Cursor.style.top = e.clientY+'px';
+		Game.Cursor.Composition.Cursor.style.left = e.clientX+'px';
+	},
+	Set: function(style=false) {
+		if (style != false) {
+			Game.Cursor.Composition.Cursor.className = 'cursor cursor_'+style;
+			document.body.appendChild(Game.Cursor.Composition.Cursor);
+			document.body.style.cursor = 'none';
+		}
+	},
+	Hide: function() {
+		Game.Cursor.Composition.Cursor.remove();
+		document.body.style.cursor = '';
+	},
+	Composition: {}
+}
+window.addEventListener('load',function(){
+	Game.CSS.Load('cursor.css');
+
+	document.body.addEventListener('mousemove',Game.Cursor.MouseMove);
+
+	Game.Cursor.Composition.Cursor = document.createElement('div');
+	Game.Cursor.Composition.Cursor.className = 'cursor cursor_magnifying-glass';
+});
