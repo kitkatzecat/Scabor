24a43844ae4006b2b8e61ca563c30e9f48b24ac2
Bryce <kitkatzecat@outlook.com>
2019-12-02 01:04:28 -0600

    Game.Sound can now preload sounds to be played

--- resources/js/sound.js
+++ resources/js/sound.js
@@ -1,21 +1,50 @@
 Game.Sound = {
-	Play: function(source) {
-		Game.Loader.Show('sound__'+source);
-		
+	Load: function(source) {
+		Game.Loader.Show('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+			
 		var audio = document.createElement("audio");
 		audio.style.display = "none";
 		audio.innerHTML = '<source src="./resources/sounds/'+source+'"></source>';
 
 		document.body.appendChild(audio);
-		
-		audio.oncanplaythrough = function() {
-			audio.play();
-			Game.Loader.Hide('sound__'+source);
-		};
-		audio.onended = function() {
-			this.parentNode.removeChild(this);
-			Game.Loader.Hide('sound__'+source);
-		};
 
+		audio.onerror = function() {
+			console.log('Game.Sound.Load: Error loading audio from source "'+source+'"');
+			Game.Loader.Hide('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+		};
+	
+		audio.oncanplaythrough = function() {
+			Game.Loader.Hide('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+		};
+		Game.Sound.Loaded[(source.substr(0,source.lastIndexOf('.')).replace('/','__'))] = audio;
+		console.log('Game.Sound.Load: Loaded audio from source "'+source+'"');
+	},
+	Loaded: {},
+	Play: function(source) {
+		if (typeof Game.Sound.Loaded[(source.substr(0,source.lastIndexOf('.')).replace('/','__'))] == 'undefined') {
+			Game.Loader.Show('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+			
+			var audio = document.createElement("audio");
+			audio.style.display = "none";
+			audio.innerHTML = '<source src="./resources/sounds/'+source+'"></source>';
+
+			document.body.appendChild(audio);
+			
+			audio.oncanplaythrough = function() {
+				audio.play();
+				Game.Loader.Hide('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+			};
+			audio.onerror = function() {
+				console.log('Game.Sound.Play: Error loading audio from source "'+source+'"');
+				Game.Loader.Hide('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+			};
+			audio.onended = function() {
+				this.parentNode.removeChild(this);
+				Game.Loader.Hide('sound__'+(source.substr(0,source.lastIndexOf('.')).replace('/','__')));
+			};
+		} else {
+			Game.Sound.Loaded[(source.substr(0,source.lastIndexOf('.')).replace('/','__'))].currentTime = 0;
+			Game.Sound.Loaded[(source.substr(0,source.lastIndexOf('.')).replace('/','__'))].play();
+		}
 	}
 };
