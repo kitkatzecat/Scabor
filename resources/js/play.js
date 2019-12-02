2d4fd8088ec56a8f85ed6e57e373268c79e63eee
Bryce <kitkatzecat@outlook.com>
2019-12-02 01:11:03 -0600

    Gametest - things to make the gametest work

--- resources/js/play.js
+++ resources/js/play.js
@@ -3,6 +3,24 @@ window.onbeforeunload = function() {
 	return 'Are you sure you want to exit? Any unsaved progress will be lost!';
 }
 
-window.addEventListener('load',function() {
-	
-});
+Game.Characters.Load('Rosemary');
+Game.Characters.Load('Simon');
+Game.Characters.Load('Me');
+
+//Game.Dialogue.Load('test.json');
+
+Game.When(function() {
+	return Game.Characters.Loaded.hasOwnProperty('Me');
+}, function(r) {
+	if (r) {
+		Game.Characters.Loaded['Me']['Name'] = 'Makoto';
+	}
+},500,10);
+
+//Game.Room.Load('hall_test.svg');
+
+Game.Sound.Load('click.mp3');
+
+Game.Story.Now = 'Chapter1';
+
+Game.Place.Load('2F_Rooms-Hall_1');
