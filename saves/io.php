62e7498c56410dc72c42d7ecd2188b9567df2005
Bryce <kitkatzecat@outlook.com>
2020-08-18 13:43:20 -0500

    Returns HTTP 404 if file is not found

--- saves/io.php
+++ saves/io.php
@@ -5,6 +5,8 @@ if ($_GET['d'] == 'list') {
 	$name = preg_replace("[^a-zA-Z0-9\_\-]",'',base64_decode($_GET['file'])).'.sav';
 	if (file_exists($name)) {
 		echo file_get_contents($name);
+	} else {
+		http_response_code(404);
 	}
 } else if ($_GET['d'] == 'write') {
 	$name = preg_replace("[^a-zA-Z0-9\_\-]",'',base64_decode($_GET['file'])).'.sav';
